7311f0247ea7c73ccf27f8372eb2f8f4260398e4
zhushengjun <1185335441@qq.com>
2018-05-30 00:48:01 +0800

    线程池基本使用

---
+++ threadpooldemo/src/main/java/com/zsj/threadpooldemo/MainActivity.java
@@ -0,0 +1,144 @@
+package com.zsj.threadpooldemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void newFixedThreadPool(View view) {
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
+
+        for (int i = 1; i <= 10; i++) {
+            final int index = i;
+            fixedThreadPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    String threadName = Thread.currentThread().getName();
+                    Log.v("zsj pool", "线程：" + threadName + ",正在执行第" + index + "个任务");
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+
+    }
+
+    public void newSingleThreadExecutor(View view) {
+        ExecutorService singleThreadPool = Executors.newSingleThreadExecutor();
+        for (int i = 1; i <= 10; i++) {
+            final int index = i;
+            singleThreadPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    String threadName = Thread.currentThread().getName();
+                    Log.v("zsjhhh", "线程：" + threadName + ",正在执行第" + index + "个任务");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    public void newCachedThreadPool(View view) {
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        for (int i = 1; i <= 10; i++) {
+            final int index = i;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            cachedThreadPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    String threadName = Thread.currentThread().getName();
+                    Log.v("zsjhhh", "线程：" + threadName + ",正在执行第" + index + "个任务");
+                    try {
+                        long time = index * 500;
+                        Thread.sleep(time);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    public void newThread(View view) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+    }
+
+    public void newScheduledThreadPool(View view) {
+        ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(3);
+        //延迟2秒后执行该任务
+        scheduledThreadPool.schedule(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, 2, TimeUnit.SECONDS);
+        //延迟1秒后，每隔2秒执行一次该任务
+        scheduledThreadPool.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, 1, 2, TimeUnit.SECONDS);
+    }
+
+    public void newSingleThreadScheduledExecutor(View view) {
+        ScheduledExecutorService singleThreadScheduledPool = Executors.newSingleThreadScheduledExecutor();
+        //延迟1秒后，每隔2秒执行一次该任务
+        singleThreadScheduledPool.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                String threadName = Thread.currentThread().getName();
+                Log.v("zsjhhh", "线程：" + threadName + ",正在执行");
+            }
+        }, 1, 2, TimeUnit.SECONDS);
+    }
+
+    public void customThreadPool(View view) {
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            ThreadManager.getNormalPool().execute(new Runnable() {
+                @Override
+                public void run() {
+                    String threadName = Thread.currentThread().getName();
+                    Log.v("zsjhhh", "线程：" + threadName + ",正在执行第" + index + "个任务");
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+    }
+}
---
+++ threadpooldemo/src/main/java/com/zsj/threadpooldemo/MyThreadPoolExecutor.java
@@ -0,0 +1,57 @@
+package com.zsj.threadpooldemo;
+
+import android.util.Log;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author 朱胜军
+ * @date 2018/5/29
+ * 描述	      扩展线程池
+ * <p>
+ * 更新者     $Author$
+ * 更新时间   $Date$
+ * 更新描述   TODO
+ */
+public class MyThreadPoolExecutor extends ThreadPoolExecutor {
+    public MyThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime,
+                                TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+    }
+
+    public MyThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
+    }
+
+    public MyThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
+    }
+
+    public MyThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+    }
+
+    @Override
+    protected void beforeExecute(Thread t, Runnable r) {
+        super.beforeExecute(t, r);
+        String threadName = t.getName();
+        Log.v("zsjhhh", "线程：" + threadName + "准备执行任务！");
+    }
+
+    @Override
+    protected void afterExecute(Runnable r, Throwable t) {
+        super.afterExecute(r, t);
+        String threadName = Thread.currentThread().getName();
+        Log.v("zsjhhh", "线程：" + threadName + "任务执行结束！");
+    }
+
+    @Override
+    protected void terminated() {
+        super.terminated();
+        Log.v("zsjhhh", "线程池结束！");
+    }
+}
