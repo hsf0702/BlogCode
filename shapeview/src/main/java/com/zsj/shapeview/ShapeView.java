0caba7c913e117c3b644596214842446d00138ed
zhushengjun <1185335441@qq.com>
2018-06-24 16:52:23 +0800

    仿58同城数据加载-（形状切换效果实现） init

---
+++ shapeview/src/main/java/com/zsj/shapeview/ShapeView.java
@@ -0,0 +1,13 @@
+package com.zsj.shapeview;
+
+/**
+ * @author 朱胜军
+ * @date 2018/6/24
+ * 描述	      TODO
+ * <p>
+ * 更新者     $Author$
+ * 更新时间   $Date$
+ * 更新描述   TODO
+ */
+public class ShapeView {
+}
